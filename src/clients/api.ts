aa4352fa366bf5f364b327e9ca582fdc250cfee2
belunatic <abelluvyou@yahoo.com>
2025-12-05 13:23:51 -0800

    feat: add baseURl for the local dev and prod dev

--- src/clients/api.ts
+++ src/clients/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const apiClient = axios.create({
-	baseURL: "http://localhost:4000",
+	baseURL: import.meta.env.VITE_BACKEND_URL,
 	headers: {
 		Authorization:
 			"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjp7Il9pZCI6IjY5MzBiNjBjYTNiNmQzZThhNGUwYzY4ZiIsInVzZXJuYW1lIjoidXNlcjEiLCJlbWFpbCI6InVzZXIxQHRlc3QuY29tIiwicm9sZSI6InVzZXIifSwiaWF0IjoxNzY0OTU2MTk3LCJleHAiOjE3NjQ5NjMzOTd9.otLU13ZjiHmXu31Mz4_lvUQdajTDeGTGYPeg3PHRzDk",
