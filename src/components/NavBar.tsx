5c2c4a55b8030f12545893546e7ec739b311c27e
belunatic <abelluvyou@yahoo.com>
2025-12-05 13:05:45 -0800

    styled the navbar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -2,7 +2,7 @@ import { NavLink } from "react-router-dom";
 
 export default function NavBar() {
 	return (
-		<nav className="flex justify-evenly items-center w-full h-10 text-white px-4 bg-red-500">
+		<nav className="flex justify-evenly items-center w-full h-10 text-white px-4 py-2 font-semibold border-b border-white">
 			<NavLink to="/"> Home </NavLink>
 			<NavLink to="/projects"> Projects </NavLink>
 		</nav>
