0d18baa6988a95fbda673f66c527b7b854f83468
belunatic <abelluvyou@yahoo.com>
2025-12-08 13:56:05 -0800

    moe form and logic to a component, clean up and redesignt he project listing

--- src/pages/ProjectsPage.tsx
+++ src/pages/ProjectsPage.tsx
@@ -2,14 +2,15 @@ import { useEffect, useState } from "react";
 import { apiClient } from "../clients/api";
 import { Link } from "react-router-dom";
 import type { Project } from "./types";
+import ProjectAddForm from "../components/projects/ProjectAddForm";
 
 export default function ProjectsPage() {
 	const [projects, setProjects] = useState<Project[]>([]);
 	const [loading, setLoading] = useState(false);
 	const [error, setError] = useState("");
 
-	const [name, setName] = useState("");
-	const [description, seDescription] = useState("");
+	// const [name, setName] = useState("");
+	// const [description, seDescription] = useState("");
 
 	useEffect(() => {
 		const fetchProjects = async () => {
@@ -30,14 +31,15 @@ export default function ProjectsPage() {
 		fetchProjects();
 	}, []);
 
-	const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-		e.preventDefault();
+	//create new Project
+	const createProject = async (name: string, description: string) => {
 		try {
 			setLoading(true);
-			const res = await apiClient.post("/api/projects", { name, description });
+			const res = await apiClient.post("/api/projects", {
+				name,
+				description,
+			});
 			setProjects((prev) => [...prev, res.data]);
-			setName("");
-			seDescription("");
 			// eslint-disable-next-line @typescript-eslint/no-explicit-any
 		} catch (error: any) {
 			console.log(error);
@@ -53,38 +55,14 @@ export default function ProjectsPage() {
 		<div>
 			<h1 className="text-3xl font-bold text-white">Projects Page</h1>
 
-			<form
-				onSubmit={handleSubmit}
-				className=" text-white border p-2 h-50 mt-10 flex flex-col gap-2 rounded">
-				<label htmlFor="project-name">Project Name: </label>
-				<input
-					type="text"
-					name="project-name"
-					className="border"
-					value={name}
-					onChange={(e) => setName(e.target.value)}
-				/>
-				<label htmlFor="project-description">Project Description</label>
-				<input
-					type="text"
-					name="project-description"
-					className="border"
-					value={description}
-					onChange={(e) => seDescription(e.target.value)}
-				/>
-				<input
-					type="submit"
-					value="Create Project"
-					className="mt-auto bg-sky-500 rounded"
-				/>
-			</form>
+			<ProjectAddForm createProject={createProject} />
 			{error && <div>{error}</div>}
-			<div className="w-full flex mt-10 gap-x-6">
+			<div className="w-full grid grid-cols-1 md:grid-cols-3 gap-2 mt-10">
 				{projects &&
 					projects.map((project) => (
 						<div
 							key={project._id}
-							className="text-white w-50 flex flex-col gap-4 border-1 border-red-500 rounded-sm p-2">
+							className="text-white flex flex-col gap-4 border border-red-500 rounded-sm p-2">
 							<h1>{project.name}</h1>
 							<p>{project.description}</p>
 							<Link
