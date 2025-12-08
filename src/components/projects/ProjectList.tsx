1d696034658156a0012e1fa85f78d97e795ae273
belunatic <abelluvyou@yahoo.com>
2025-12-08 14:10:27 -0800

    add type for props, moved logic from project page to a component

---
+++ src/components/projects/ProjectList.tsx
@@ -0,0 +1,27 @@
+import type { Project } from "../../pages/types";
+import { Link } from "react-router-dom";
+
+interface ProjectListProps {
+	projects: Project[];
+}
+
+export default function ProjectList({ projects }: ProjectListProps) {
+	return (
+		<div className="w-full grid grid-cols-1 md:grid-cols-3 gap-2 mt-10">
+			{projects &&
+				projects.map((project) => (
+					<div
+						key={project._id}
+						className="text-white flex flex-col gap-4 border border-red-500 rounded-sm p-2">
+						<h1>{project.name}</h1>
+						<p>{project.description}</p>
+						<Link
+							to={`/projects/${project._id}`}
+							className="mt-auto bg-sky-500 rounded text-center cursor-pointer">
+							See Project
+						</Link>
+					</div>
+				))}
+		</div>
+	);
+}
