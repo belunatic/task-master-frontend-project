0907a3e3eba300cda55a498c7e15e60b221ab598
belunatic <abelluvyou@yahoo.com>
2025-12-08 15:34:29 -0800

    add functionality to prefill edit form, and close the form

--- src/pages/ProjectsPage.tsx
+++ src/pages/ProjectsPage.tsx
@@ -3,11 +3,18 @@ import { apiClient } from "../clients/api";
 import type { Project } from "./types";
 import ProjectAddForm from "../components/projects/ProjectAddForm";
 import ProjectList from "../components/projects/ProjectList";
+import ProjectEditForm from "../components/projects/ProjectEditForm";
 
 export default function ProjectsPage() {
 	const [projects, setProjects] = useState<Project[]>([]);
 	const [loading, setLoading] = useState(false);
 	const [error, setError] = useState("");
+
+	//edit form state
+	const [editId, setEditId] = useState("");
+	const [editName, setEditName] = useState("");
+	const [editDescription, setEditDescription] = useState("");
+	const [showEditForm, setShowEditForm] = useState(false);
 
 	useEffect(() => {
 		const fetchProjects = async () => {
@@ -46,6 +53,52 @@ export default function ProjectsPage() {
 		}
 	};
 
+	//update new Project
+	const updateProject = async (
+		id: string,
+		name: string,
+		description: string
+	) => {
+		try {
+			setLoading(true);
+			const res = await apiClient.put(`/api/projects/${id}`, {
+				name,
+				description,
+			});
+			setProjects((prev) =>
+				prev.map((project) => {
+					return project._id === id
+						? { ...project, name, description }
+						: project;
+				})
+			);
+			console.log(res.data);
+			// eslint-disable-next-line @typescript-eslint/no-explicit-any
+		} catch (error: any) {
+			console.log(error);
+			setError(error.message);
+		} finally {
+			setLoading(false);
+		}
+	};
+
+	//set up the update project values
+	const editProject = (id: string, name: string, description: string) => {
+		setEditName(name);
+		setEditDescription(description);
+		setEditId(id);
+		setShowEditForm(true);
+		console.log("This is a form edit form");
+	};
+
+	//clear the edit form and close it
+	const closeEditForm = () => {
+		setEditName("");
+		setEditDescription("");
+		setEditId("");
+		setShowEditForm(false);
+	};
+
 	if (loading) return <div className="text-2xl text-white">Loading</div>;
 
 	return (
@@ -55,7 +108,16 @@ export default function ProjectsPage() {
 			<ProjectAddForm createProject={createProject} />
 			{error && <div>{error}</div>}
 
-			<ProjectList projects={projects} />
+			{showEditForm && (
+				<ProjectEditForm
+					editId={editId}
+					editName={editName}
+					editDescription={editDescription}
+					updateProject={updateProject}
+					closeEditForm={closeEditForm}
+				/>
+			)}
+			<ProjectList projects={projects} editProject={editProject} />
 		</div>
 	);
 }
