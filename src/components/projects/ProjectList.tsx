9ad9d23c82cb6cc0625647a9d1f08db279cdd698
belunatic <abelluvyou@yahoo.com>
2025-12-08 16:11:20 -0800

    add logic to delete project from the list with confirmation box

--- src/components/projects/ProjectList.tsx
+++ src/components/projects/ProjectList.tsx
@@ -1,15 +1,37 @@
+import { useState } from "react";
 import type { Project } from "../../pages/types";
 import { Link } from "react-router-dom";
 
 interface ProjectListProps {
 	projects: Project[];
 	editProject: (id: string, name: string, description: string) => void;
+	deleteProject: (id: string) => void;
 }
 
 export default function ProjectList({
 	projects,
 	editProject,
+	deleteProject,
 }: ProjectListProps) {
+	const [confirm, setConfirm] = useState(false);
+	const [DeleteId, setDeleteId] = useState("");
+
+	//delete project
+	const handleDelete = (id: string) => {
+		deleteProject(id);
+		setConfirm(false);
+	};
+
+	//open the only clicked confirmBox
+	const openConfirmBox = (id: string) => {
+		setDeleteId(id);
+		setConfirm(true);
+	};
+
+	const closeConfirmBox = () => {
+		setDeleteId("");
+		setConfirm(false);
+	};
 	return (
 		<div className="w-full grid grid-cols-1 md:grid-cols-3 gap-2 mt-10">
 			{projects &&
@@ -32,6 +54,30 @@ export default function ProjectList({
 							className="mt-auto bg-green-500 rounded text-center cursor-pointer">
 							Edit
 						</button>
+						{!confirm || project._id !== DeleteId ? (
+							<button
+								type="button"
+								onClick={() => openConfirmBox(project._id)}
+								className="mt-auto bg-red-500 rounded text-center cursor-pointer">
+								delete
+							</button>
+						) : (
+							<div className="flex flex-col gap-y-2 justify-center">
+								<p className="text-center">Are you sure?</p>
+								<div className="flex gap-x-2">
+									<button
+										className="bg-red-500 p-2 w-full cursor-pointer"
+										onClick={() => handleDelete(project._id)}>
+										Yes
+									</button>
+									<button
+										className="bg-green-500 p-2 w-full cursor-pointer"
+										onClick={() => closeConfirmBox}>
+										No
+									</button>
+								</div>
+							</div>
+						)}
 					</div>
 				))}
 		</div>
