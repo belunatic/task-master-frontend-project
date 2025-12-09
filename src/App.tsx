5fa0964c6f08638666b45c40193666b02c15e144
belunatic <abelluvyou@yahoo.com>
2025-12-09 11:13:45 -0800

    add tags and stlying for the pages

--- src/App.tsx
+++ src/App.tsx
@@ -6,16 +6,21 @@ import ProjectDetailsPage from "./pages/ProjectDetailsPage";
 
 function App() {
 	return (
-		<>
-			<div className="p-5 bg-zinc-900 h-min-screen">
-				<NavBar />
-				<Routes>
-					<Route path="/" element={<HomePage />} />
-					<Route path="/projects" element={<ProjectsPage />} />
-					<Route path="/projects/:projectId" element={<ProjectDetailsPage />} />
-				</Routes>
-			</div>
-		</>
+		<main className=" bg-zinc-900 text-white h-full md:h-screen">
+			<section className=" max-w-[85rem] mx-auto flex flex-col gap-y-4">
+				<div className="p-5 bg-zinc-900 h-min-screen">
+					<NavBar />
+					<Routes>
+						<Route path="/" element={<HomePage />} />
+						<Route path="/projects" element={<ProjectsPage />} />
+						<Route
+							path="/projects/:projectId"
+							element={<ProjectDetailsPage />}
+						/>
+					</Routes>
+				</div>
+			</section>
+		</main>
 	);
 }
 
